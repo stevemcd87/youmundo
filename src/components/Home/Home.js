f11b75fffe7c62654abaf88932904b887f6ee9ea
Natalia Paguaga <npaguaga12@gmail.com>
2018-06-25 04:35:34 -0400

    pushing final project

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,15 +5,21 @@ import PropTypes from 'prop-types';
 import MiniDrawer from '../MiniDrawer.js'
 import SimpleMediaCard from '../SimpleMediaCard.js'
 import Grid from '@material-ui/core/Grid';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
-// import PropTypes from 'prop-types';
+
+
+
+
 
 
 let recommendedList = [];
 let videosClicked = [];
 let lsBoolean;
-let lsList;
-let searchList;
+let lsVideoList = [];
+let lsSearchedList = [];
 
 
 class Home extends Component {
@@ -23,13 +29,20 @@ class Home extends Component {
 		this.state = {
 			currentVideoInfo: {},
 			usersLSvideosClicked: [],
-			usersLSsearches:[]
+			usersLSsearches:[],
+			usersRecommendedList: []
 		}
 	}
 
 	componentWillMount () {
-		this.state.usersLSvideosClicked = JSON.parse(localStorage.getItem("videosClicked"));
-		this.state.usersLSsearches = JSON.parse(localStorage.getItem("userSearches"));
+		this.setState({
+			usersLSvideosClicked: JSON.parse(localStorage.getItem("videosClicked"))
+		})
+		this.setState({
+			usersLSsearches: JSON.parse(localStorage.getItem("userSearches"))
+		})
+		// console.log(this.state);
+
 		this.state.currentVideoInfo = {
 		 "title": "\"Abrazos no muros\", el amor invade la frontera México-EEUU",
 		 "description": "La frontera entre EEUU y México se abrió para que 100 familias se reencontraran después de muchos años por apenas tres minutos",
@@ -43,7 +56,7 @@ class Home extends Component {
 		 ],
 		 "image": "http://stage.telemundo.com/sites/nbcutelemundo/files/images/promo/video_clip/2016/08/11/amor-invade-frontera-de-mexico-y-eeuu.jpg"
 		};
-		(lsList ) ? lsBoolean=true : lsBoolean = false;
+		(lsVideoList ) ? lsBoolean=true : lsBoolean = false;
 	}
 
 	// componentWillUpdate(){
@@ -54,14 +67,14 @@ class Home extends Component {
 	// 	}
 	// }
 
-	// componentDidMount(){
-	// 	console.log(this.state);
-	// }
+	componentDidMount(){
+		console.log(this.state);
+	}
 
 	returnIfMatched(mediaId){
 		let matched = false;
 		if (lsBoolean ) {
-			lsList.forEach((val)=>{
+			lsVideoList.forEach((val)=>{
 				matched = false;
 				if(val === mediaId) {
 					matched = true;
@@ -79,8 +92,8 @@ class Home extends Component {
 			currentVideoInfo: videoInfo
 		});
 		const currentState = this.state;
-		videosClicked.push(currentState.currentVideoInfo);
-		videosClicked = Array.from(new Set(videosClicked));
+		videosClicked.unshift(videoInfo);
+		videosClicked = Array.from(new Set(videosClicked)).slice(0,2);
 		localStorage.setItem('videosClicked', JSON.stringify(videosClicked));
 		this.setState({
 			usersLSvideosClicked: videosClicked
@@ -91,29 +104,43 @@ class Home extends Component {
 	compareSearchesToKeywords(data){
 		const searchedWords =  this.state.usersLSsearches;
 		data.map((val, ind)=>{
-				if (ind === 0 ){
+			// console.log("val");
+			// console.log(val);
+				if (val.keywords ){
 					val.keywords.forEach((val2)=>{
-						searchedWords.forEach((val3)=>{
-							if(val3 === val2){
-								const valString = JSON.stringify(val);
-								recommendedList.push(valString);
-								recommendedList = Array.from(new Set(recommendedList));
-							}
-						})
-
+						// console.log("val2");
+						// console.log(val2);
+						if (searchedWords){
+							searchedWords.forEach((val3)=>{
+								if(val3 === val2){
+									// console.log("val3");
+									// console.log(val3);
+									const valString = JSON.stringify(val);
+									recommendedList.push(valString);
+									recommendedList = Array.from(new Set(recommendedList));
+									// recommendedList = JSON.parse(recommendedList);
+									console.log(recommendedList);
+									// return recommendedList;
+									// this.setState({
+									// 	usersRecommendedList: recommendedList
+									// })
+								}
+							})
+						}
 					})
+				} else {
+					// console.error(val);
 				}
 			})
-			console.log("recommendedList=============");
-			console.log(recommendedList);
-			return recommendedList
+			// console.log("recommendedList=============");
+			// console.log(recommendedList);
+			// return recommendedList
 
 	}
 
 
 	Item(videoInfo) {
   return (
-
 		<ul onClick={(e)=> this.handleVidClick(videoInfo)} key={JSON.stringify(videoInfo)} >
 			<li key={videoInfo.title}>{videoInfo.title}</li>
 			<img alt= {videoInfo.title} key= {videoInfo.image} src= {videoInfo.image} className="list-image"></img>
@@ -123,166 +150,151 @@ class Home extends Component {
 
 	recentlyViewed(videoInfo){
 		return (
-			<ul>
+			<ul key={videoInfo.permalink}>
 				{
 						<li  onClick={(e) => this.handleVidClick(videoInfo) }>
-
 						 <img src= {videoInfo.image} height="100" width="100"></img>
 					 </li>
 					}
 			</ul>
 		)
 	}
-	// recommendedList(videoInfo){
-	// 	return (
-	// 		<ul>
-	// 			{
-	// 					<li  onClick={(e) => this.(videoInfo) }>
-	//
-	// 					 <img src= {videoInfo.image} height="100" width="100"></img>
-	// 				 </li>
-	// 				}
-	// 		</ul>
-	// 	)
-	// }
 
 	render() {
-		return (
-
+	return (
+		<div>
 			<MiniDrawer>
+				<ReactiveBase
+					app="YouMundo"
+					credentials="6Ook2nnnU:1e9d454b-f3d2-4b8c-96f2-e25a0f84969b"
+				>
+					<DataSearch
+						className="search-bar"
+						width="80%"
+						componentId="SearchSensor"
+						dataField={["keywords", "title", "description"]}
+						onValueSelected={function(value) {
+								lsSearchedList.push(value);
+								lsSearchedList = Array.from(new Set(lsSearchedList));
+								localStorage.setItem('userSearches', JSON.stringify(lsSearchedList));
+								this.setState({usersLSsearches: lsSearchedList});
+						}.bind(this)} />
+					<Grid container spacing={16}>
+						<Grid item sm={6} >
+							<ReactMpxPlayer
+								 className="GallerySliderVideo"
+								 width="100%"
+								 height="400px"
+								 style={{marginTop: '10px'}}
+								 src={`https://player.theplatform.com/p/0L7ZPC/D7AjRZyan6zo/embed/select/${this.state.currentVideoInfo.mediaId}?autoPlay=true&mute=false`}
+								 allowFullScreen
+								 onLoad={() => {
+									 console.log('Player is Loaded!')
+								 }}
+								 onPdkControllerInstalled={pdkController => {
+									 pdkController.addEventListener('OnMediaStart', () => {
+										 console.log('Media has started!');
+										 // Pause the video after 10 seconds of playing
+										 setTimeout(() => pdkController.pause(true), 10000);
+									 })
+								 }}
+							 />
+						</Grid>
+					{this.state.usersLSvideosClicked &&
+						<Grid item sm={6} >
+							<h3>Recently Watched</h3>
+							<Grid container spacing={16} >
+								{this.state.usersLSvideosClicked.map((video) => {
+									return(
+										<Grid item xs={6} onClick={(e) => {this.handleVidClick(video)}}>
+											<ListItem button><SimpleMediaCard title={video.title} image={video.image} /></ListItem>
+										</Grid>
+									)
+								})}
+							</Grid>
+						</Grid>
+					}
+					</Grid>
+					{
+						<ReactiveList
+							className="video-list"
+							componentId="SearchResult"
+							dataField="title"
+							loader="Loading Results.."
+							size={120}
+							onAllData={
+								(res) => {
 
+									return(
 
-			<ReactiveBase
-				app="YouMundo"
-				credentials="6Ook2nnnU:1e9d454b-f3d2-4b8c-96f2-e25a0f84969b">
-				<DataSearch
-					className="search-bar"
-					width="80%"
-				  componentId="SearchSensor"
-				  dataField={["keywords", "title", "description"]}
-					onValueSelected={function(value) {
-							recommendedList.push(value);
-							recommendedList = Array.from(new Set(recommendedList));
-							localStorage.setItem('userSearches', JSON.stringify(recommendedList));
-							console.log(recommendedList);
-							console.log("recommendedList================");
+										<Grid container spacing={24}>
+											{ this.compareSearchesToKeywords(res)}
+											{
+												res.map( (results, ind) => {
+													if(ind < 4){
+														return(
+														<Grid
+															key={results.mediaId}
+															item xs={6}
+															sm={3}
+															onClick={(e) => {this.handleVidClick(results)}}
+														>
+															{
+															<ListItem button>
+																<SimpleMediaCard
+																	title={results.title}
+																	image={results.image}
+																/>
+															</ListItem>
+															}
+														</Grid>
+													)}
 
-							// this.handleSearch(recommendedList);
-	 					}
- 					}
-				/>
-				<ReactMpxPlayer
-					 className="GallerySliderVideo"
-					 height="80%"
-					 width="80%"
-					 src={`https://player.theplatform.com/p/0L7ZPC/D7AjRZyan6zo/embed/select/${this.state.currentVideo}?autoPlay=true&mute=false`}
-					 allowFullScreen
-					 onLoad={() => {
-						 console.log('Player is Loaded!')
-					 }}
-					 onPdkControllerInstalled={pdkController => {
-						 pdkController.addEventListener('OnMediaStart', () => {
-							 console.log('Media has started!');
-							 // Pause the video after 10 seconds of playing
-							 setTimeout(() => pdkController.pause(true), 10000);
-						 })
-					 }}
-				 />
-
-				<ReactiveList
-					className="video-list"
-				  componentId="SearchResult"
-				  dataField="title"
-				  loader="Loading Results.."
-					size = {1000}
-				  onAllData={
-						(res) =>
-						 <div className= "body">
-							 <div className= "regular">
-
-								 <h1>Regular</h1>
-								 <div className= "list">
-									 {res.map((object, ind) => {
-										 if(ind < 5) {
-											 return (
-												 this.Item(object)
-											 )
-										 }
-									 }
-									)}
-								</div>
-							 </div>
-								{this.state.usersLSvideosClicked &&
-								 <div className= "recently-viewed">
-									 <h1>Recently Viewed</h1>
-									 <div className="list">
-										 { this.state.usersLSvideosClicked.map((object, ind) => {
-											 if(ind < 5) {
-												 return (
-													 this.recentlyViewed(object)
-												 )
-											 }
-										 }
-									 )}
-									 </div>
-								 </div>
-								}
-								{/* {this.state.usersLSsearches &&
-									<div className= "recommended-list">
-										<h1>Recommended</h1>
-										<div className="list">
-											{ recommendedList.map((object, ind) => {
-												if(ind < 5) {
-													return (
-														this.recentlyViewed(object)
-													)
-												}
+												})
 											}
-										)}
-										</div>
-									</div>
-							} */}
+											{this.state.usersLSsearches &&
+												<div>
+													<h1>Recommended</h1>
+												<Grid container spacing={16}>
 
-						 </div> //END OF BODY
 
-						}
-				  react={{
-				    and: ["SearchSensor"]
-				  }}
-				/>
-			</ReactiveBase>
+														{
+							 							recommendedList.map((object, ind) => {
+															const parsedObject = JSON.parse(object);
+															return (
+																<Grid
+																	key={parsedObject.mediaId}
+																	item xs={3}
+
+																	onClick={(e) => {this.handleVidClick(parsedObject)}}
+																>
+																	<ListItem button><SimpleMediaCard title={parsedObject.title} image={parsedObject.image} /></ListItem>
+																</Grid>
+
+															)
+
+														}
+													)}
+
+												</Grid>
+											</div>
+
+									}
+
+										</Grid>
+
+									)
+								}
+							}
+
+							react={{ and: ["SearchSensor"] }}
+						/>
+					}
+				</ReactiveBase>
 			</MiniDrawer>
-
-// 					 <ReactiveList
-// 						 className="video-list"
-// 						 componentId="SearchResult"
-// 						 dataField="title"
-// 						 loader="Loading Results.."
-// 						 onData={
-// 							 (res) =>
-							 	
-// 								 	<SimpleMediaCard
-// 				           	key={res.mediaId}
-// 									 	title={res.title}
-// 				           	keywords={res.keywords}
-// 				           	onClick={(e) => this.handleVidClick(res.mediaId) }
-// 				           	image={res.image}
-// 				           	description={res.description}
-// 				         	/>
-
-
-// 						 }
-// 						 onResultStats={(total, took) => {
-// 							 return "found " + total + " results in " + took + "ms."
-// 						 }}
-// 						 react={{
-// 							 and: ["SearchSensor"]
-// 						 }}
-// 					 />
-
-
-		) // End of render return;
-	}
+		</div>
+	);
+}
 }
 
 // Home.propTypes = {
