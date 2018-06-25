95de5c2114e8964a1e143aa265c7b72cd0e3aef1
Natalia Paguaga <npaguaga12@gmail.com>
2018-06-25 13:01:40 -0400

    changed auth0 callback

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -17,7 +17,7 @@ export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'youmundo.auth0.com',
     clientID: '4KuyASwpsANZiiSYicUdgYM632Q-kImj',
-    redirectUri: 'http://localhost:3000/callback',
+    redirectUri: 'https://youmundo.herokuapp.com/callback',
     audience: 'https://youmundo.auth0.com/userinfo',
     responseType: 'token id_token',
     scope: 'openid profile'
