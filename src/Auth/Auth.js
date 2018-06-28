4799ecdc60da36d66e622c1a0c12bb96dec3b674
Stephen McDonald <stephenmcdonald8787@gmail.com>
2018-06-28 12:59:34 -0400

    updated auth

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -17,7 +17,7 @@ export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'youmundo.auth0.com',
     clientID: '4KuyASwpsANZiiSYicUdgYM632Q-kImj',
-    redirectUri: 'http://youmundo.heroku.com/callback',
+    redirectUri: 'https://youmundo.heroku.com/callback',
     audience: 'https://youmundo.auth0.com/userinfo',
     responseType: 'token id_token',
     scope: 'openid profile'
